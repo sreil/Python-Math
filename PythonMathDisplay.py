de5b6013f2dc8c82fdca6708b6faf75d4ccdffea
sreil <seanpreilly@gmail.com>
2015-09-03 12:10:46 -0600

    add py math

---
+++ PythonMathDisplay.py
@@ -0,0 +1,26 @@
+#python math
+
+# arithmetic operators
+# +		plus		addition
+# -		minus		subtraction
+# *		times		multiplication
+# /		divided by 	division
+# **    power		exponentiation
+
+print 12, -3, 3.14159, -1.8 #prints 12 -3 3.14159 -1.8
+
+print type(1), type(3.14159), type(2.0) # prints <type 'int'> <type 'float'> <type 'float'>
+
+print int(3.14159), int(-5.5) # prints 3 -5
+
+print float(7), float(-5) # prints 7.0 -5.0
+
+print 3.1415926535897932384626433832795028841971 #python prints out 12 characters
+
+print 1.0 / 3, 7.0 / 2.0, -8 / 3.0 # prints 0.3333333333 3.5 -2.333333333333  
+
+print 1 + 2, 3 - 4, 5 * 6, 7 ** 8 ## prints 3 -1 30 5764801
+
+print 1 * 2 + 3 * 4 #prints 14
+
+print 1 * (2 + 3) * 4 # prints 20
